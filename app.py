f54a525634792a3c5a1001133163999f1d765b8e
RamPatel04 <122635290+RamPatel04@users.noreply.github.com>
2024-01-14 07:00:30 -0500

    Update app.py
    adding flask functionality

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from main import main
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/generate', methods=['POST'])
+def story_image_generator():
+    prompt = request.json['prompt']
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/post', methods=['POST'])
+def post():
+    return "recived: {}".format(request.form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
